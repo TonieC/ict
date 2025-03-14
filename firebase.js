26b7dfbd46e18ad9015f2fb38789189427a0d800
TonieC <jesstonievlogs@gmail.com>
2025-03-14 18:57:47 +0800

    Added Firebase Integration

---
+++ firebase.js
@@ -0,0 +1,20 @@
+// Import Firebase modules
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc, query, orderBy, onSnapshot } from "firebase/firestore"; 
+
+// Your Firebase project configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAl9OlVDnZZpZwcWft9_jvdn_2VDpiBKbg",
+    authDomain: "camarinians-wall.firebaseapp.com",
+    projectId: "camarinians-wall",
+    storageBucket: "camarinians-wall.firebasestorage.app",
+    messagingSenderId: "1019298262602",
+    appId: "1:1019298262602:web:6aaa132d872515fc8580b3",
+    measurementId: "G-5V0E174JPS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db, collection, addDoc, query, orderBy, onSnapshot };
