3734f53f478921b01701a94859f389600d4c938d
TonieC <jesstonievlogs@gmail.com>
2025-03-14 19:17:16 +0800

    Added Firestore integration for messages

---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,8 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'ict',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
+
---
+++ node_modules/@firebasegen/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'ict',
+  location: 'us-central1'
+};
+
